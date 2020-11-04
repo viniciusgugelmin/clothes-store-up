6c1c80cab52c7724787c26c6d374ece4f82988c6
Vinicius <vinigugelmin@gmail.com>
2020-11-03 21:11:00 -0300

    Modifications in insert into payment methods table SQL

--- src/com/github/clothesstore/database/sql/03112010_insert_values_into_payment_methods_table.sql
+++ src/com/github/clothesstore/database/sql/03112010_insert_values_into_payment_methods_table.sql
@@ -1,5 +1,7 @@
 use clothes_store;
 
+delete from `payment_methods`;
+alter table `payment_methods` AUTO_INCREMENT = 1;
 insert into `payment_methods`(`type`) values
 	('BANK_SLIP'),
     ('CREDIT_CARD');
